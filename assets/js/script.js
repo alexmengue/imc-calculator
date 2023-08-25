a0f2fc006514cc5e6a729f209572985b9409f445
Alex <alexmengue1990@hotmail.com>
2023-08-25 14:46:34 -0300

    finished with responsive layout

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,12 +11,16 @@ form.addEventListener('submit', (event) => {
     const value = document.getElementById('value');
     let description = '';
 
+    value.classList.add('attention');
+
     document.getElementById('infos').classList.remove('hidden');
 
     if (imc < 18.5) {
         description = 'Cuidado! Você está abaixo do peso!';
     } else if (imc >= 18. && imc <= 25) {
         description = 'Você está no peso ideal!';
+        value.classList.remove('attention');
+        value.classList.add('normal');
     } else if (imc > 25 && imc <= 30) {
         description = 'Cuidado! Você está com sobrepeso';
     } else if (imc > 30 && imc <= 35) {
